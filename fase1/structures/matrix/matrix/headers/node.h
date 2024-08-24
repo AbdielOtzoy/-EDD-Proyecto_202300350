ad5e1795ee979024e3f4adae2267c3fa648d7475
Abdiel Otzoy <abdielotzoy@gmail.com>
2024-08-24 15:27:07 -0600

    matriz dispersa

---
+++ fase1/structures/matrix/matrix/headers/node.h
@@ -0,0 +1,16 @@
+
+class MatrixNode
+{
+private:
+  /* data */
+public:
+  MatrixNode(int col, int row, int value);
+
+  int value;
+  int col;
+  int row;
+  MatrixNode *up;
+  MatrixNode *bottom;
+  MatrixNode *right;
+  MatrixNode *left;
+};
